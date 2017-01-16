247b003227cac74ccb601aa72fd91decb3e07737
Eastonboy99 <tyler@gwins.net>
2017-01-16 12:05:30 -0500

    Added Motor Controller Project and Arcade Drive Project

---
+++ Arcade Driving/src/org/usfirst/frc/team6484/robot/Robot.java
@@ -0,0 +1,36 @@
+package org.usfirst.frc.team6484.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.Timer;
+
+/**
+ * This sample program shows how to control a motor using a joystick. In the
+ * operator control part of the program, the joystick is read and the value is
+ * written to the motor.
+ *
+ * Joystick analog values range from -1 to 1 and speed controller inputs also
+ * range from -1 to 1 making it easy to work together. The program also delays a
+ * short time in the loop to allow other threads to run. This is generally a
+ * good idea, especially since the joystick values are only transmitted from the
+ * Driver Station once every 20ms.
+ */
+public class Robot extends SampleRobot {
+
+	
+	public Robot() {
+	}
+
+	/**
+	 * Runs the motor from a joystick.
+	 */
+	@Override
+	public void operatorControl() {
+		while (isOperatorControl() && isEnabled()) {
+			myDrive.arcadeDrive(driveStick);
+			Timer.delay(.01);
+		}
+	}
+}
