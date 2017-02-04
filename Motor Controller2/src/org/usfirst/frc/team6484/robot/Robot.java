d94b653891320b1a7b2de46260a4ef824438e771
Eastonboy99 <tyler@gwins.net>
2017-02-03 20:28:52 -0500

    Added motor Controller 2 and Pneumatic test

--- Motor Controller2/src/org/usfirst/frc/team6484/robot/Robot.java
+++ Motor Controller2/src/org/usfirst/frc/team6484/robot/Robot.java
@@ -1,59 +1,45 @@
 package org.usfirst.frc.team6484.robot;
 
-import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.XboxController;
 
 /**
  * This sample program shows how to control a motor using a joystick. In the
  * operator control part of the program, the joystick is read and the value is
  * written to the motor.
  *
- * Joystick analog val7making it easy to work together. The program also delays a
+ * Joystick analog values range from -1 to 1 and speed controller inputs also
+ * range from -1 to 1 making it easy to work together. The program also delays a
  * short time in the loop to allow other threads to run. This is generally a
  * good idea, especially since the joystick values are only transmitted from the
  * Driver Station once every 20ms.
  */
 public class Robot extends SampleRobot {
 
-	 // initialize the motor as a
-	Compressor c = new Compressor(0);
-	DoubleSolenoid exampleDouble = new DoubleSolenoid(0, 1);
+	private SpeedController motor = new Talon(0); // initialize the motor as a
 													// Talon on channel 0
-	private XboxController stick = new XboxController(1); // initialize the joystick on port
+	private Joystick stick = new Joystick(0); // initialize the joystick on port
 												// 0
 
 	private final double kUpdatePeriod = 0.005; // update every 0.005 seconds/5
 												// milliseconds (200Hz)
 
 	public Robot() {
-		c.setClosedLoopControl(false);
 	}
 
-	/**                                                                                                                                        
+	/**
 	 * Runs the motor from a joystick.
 	 */
 	@Override
 	public void operatorControl() {
 		while (isOperatorControl() && isEnabled()) {
-			
-			if (stick.getStartButton())
-				c.setClosedLoopControl(true);
-			else if (stick.getBackButton())
-				c.setClosedLoopControl(false);
-			
-			if (stick.getAButton())
-				exampleDouble.set(DoubleSolenoid.Value.kForward);
-			else if (stick.getBButton())
-				exampleDouble.set(DoubleSolenoid.Value.kReverse);
-			else
-				exampleDouble.set(DoubleSolenoid.Value.kOff);
-			
+			// Set the motor's output.
+			// This takes a number from -1 (100% speed in reverse) to +1 (100%
+			// speed going forward)
+			motor.set(stick.getY());
 
 			Timer.delay(kUpdatePeriod); // wait 5ms to the next update
 		}
